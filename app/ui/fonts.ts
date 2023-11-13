c78770517a33d9727a5a889476d816d9ac6ff1f8
unknown <unknown@unknown.unknown>
2023-11-13 19:01:34 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Plus_Jakarta_Sans, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
